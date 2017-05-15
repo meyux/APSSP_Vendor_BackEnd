c8f981ae914085d2c1bd245ae16e42348df3c2e2
meyux <meyuxx@gmail.com>
2017-05-15 23:40:57 +0800

    changes on URI for status with json types

--- src/main/java/com/apssp/vendor/backend/service/StatusMasterFacadeREST.java
+++ src/main/java/com/apssp/vendor/backend/service/StatusMasterFacadeREST.java
@@ -25,7 +25,7 @@ import javax.ws.rs.core.MediaType;
  * @author meyux
  */
 @Stateless
-@Path("com.apssp.vendor.backend.statusmaster")
+@Path("status")
 public class StatusMasterFacadeREST extends AbstractFacade<StatusMaster> {
 
     @PersistenceContext(unitName = "com.apssp.vendor.backend_APSSP_Vendor_BackEnd_war_1.0-SNAPSHOTPU")
@@ -37,14 +37,14 @@ public class StatusMasterFacadeREST extends AbstractFacade<StatusMaster> {
 
     @POST
     @Override
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_JSON})
     public void create(StatusMaster entity) {
         super.create(entity);
     }
 
     @PUT
     @Path("{id}")
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_JSON})
     public void edit(@PathParam("id") Integer id, StatusMaster entity) {
         super.edit(entity);
     }
@@ -57,21 +57,21 @@ public class StatusMasterFacadeREST extends AbstractFacade<StatusMaster> {
 
     @GET
     @Path("{id}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
     public StatusMaster find(@PathParam("id") Integer id) {
         return super.find(id);
     }
 
     @GET
     @Override
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
     public List<StatusMaster> findAll() {
         return super.findAll();
     }
 
     @GET
     @Path("{from}/{to}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
     public List<StatusMaster> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
         return super.findRange(new int[]{from, to});
     }
